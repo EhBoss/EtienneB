c3439f5ac6d9cc8fed2443f9f79890c774a72c70
EhBoss <43215408+EhBoss@users.noreply.github.com>
2021-05-02 21:13:16 +0200

    Modification CSS

--- index.php
+++ index.php
@@ -10,21 +10,19 @@
     <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/style.css"/>
 </head>
 <body>
-    <section class="container">
-        <div class="item-a">item-a</div>
-        <div class="item-b">item-b</div>
-        <div class="item-c">item-c</div>
-        <div class="item-d">item-d</div>
-        <div class="item-e">item-e</div>
-    </section>
-
-    <main id="container" display:grid>
-        <h1 class="section-title">Experiences</h1>
-        <section class="IT">
-            <h2 class="section-title">IT</h2>
-            <h4 class="date-title">Octobre 2019 – Aujourd’hui</h4> 
-            <h5 class="place-title">Nantes, FR</h5>
-            <h3 class="job-title">Ingénieur Études et développement</h3>
+    <main class="container">
+        <header>
+            <?php include_once('navbar.php');?>
+        </header>
+        
+        <section class="experience-IT">
+            <h1 class="section-title">Experiences</h1>
+            <div>
+                <h2 class="section-title">IT</h2>
+                <h4 class="date-title">Octobre 2019 – Aujourd’hui</h4> 
+                <h5 class="place-title">Nantes, FR</h5>
+                <h3 class="job-title">Ingénieur Études et développement</h3>
+            </div>
 
             <h2 class="project">Projet ENEDIS (EDF) : (À partir de juillet 2020)</h2>
             <p class="project-definition">Traitement et affichage des données liées aux Postes sources. Analyse et correctifs d’anomalies, développement PHP pour évoluer l’application.</p>
@@ -46,12 +44,14 @@
                 <li class="tool-list"><strong>Environnements : </strong>Eclipse, VS Code, Méthodologie AGILE, PHP Storm</li>
             </ul>
         </section>
-        <section class="industrie">
-            <h2 class="section-title">Industrie</h2>
-            <h2 class="company-title">TDC</h2>
-            <h4 class="date-title">2016 – 2018</h4> 
-            <h5 class="place-title">Timisoara, RO</h5>
-            <h3 class="job-title">Chef de Projet Plasturgie</h3>
+        <section class="experience-industrie">
+            <div>
+                <h2 class="section-title">Industrie</h2>
+                <h2 class="company-title">TDC</h2>
+                <h4 class="date-title">2016 – 2018</h4> 
+                <h5 class="place-title">Timisoara, RO</h5>
+                <h3 class="job-title">Chef de Projet Plasturgie</h3>
+            </div>
 
             <h2 class="project">Industrie Automobile – rang 2</h2>
             <p class="project-definition">Traitement et affichage des données liées aux Postes sources. Analyse et correctifs d’anomalies, développement PHP pour évoluer l’application.</p>
@@ -65,10 +65,12 @@
                 <li class="action">LEAN Management (5S – PDCA – 5 Pourquoi ?)</li>
             </ul>
             
-            <h2 class="company-title">Aptar</h2>
-            <h4 class="date-title">2013 – 2016</h4> 
-            <h5 class="place-title">Le Vaudreuil, FR</h5>
-            <h3 class="job-title">Ingénieur Méthodes en apprentissage</h3>
+            <div>
+                <h2 class="company-title">Aptar</h2>
+                <h4 class="date-title">2013 – 2016</h4> 
+                <h5 class="place-title">Le Vaudreuil, FR</h5>
+                <h3 class="job-title">Ingénieur Méthodes en apprentissage</h3>
+            </div>
 
             <h2 class="project">Industrie Pharmaceutique</h2>
 
@@ -78,10 +80,12 @@
                 <li class="action">Pilote du chantier 5S des ensembles de préhension (amélioration +15%)</li>
             </ul>
             
-            <h2 class="company-title">Cooper Standard Automotive</h2>
-            <h4 class="date-title">2011 – 2013</h4> 
-            <h5 class="place-title">Rennes, FR</h5>
-            <h3 class="job-title">Technicien plasturgie en apprentissage</h3>
+            <div>
+                <h2 class="company-title">Cooper Standard Automotive</h2>
+                <h4 class="date-title">2011 – 2013</h4> 
+                <h5 class="place-title">Rennes, FR</h5>
+                <h3 class="job-title">Technicien plasturgie en apprentissage</h3>
+            </div>
 
             <h2 class="project">Industrie Automobile – rang 1</h2>
 
@@ -92,10 +96,12 @@
 
             <h2 class="section-title">Stages internationaux : </h2>
 
-            <h2 class="company-title">Aptar</h2>
-            <h4 class="date-title">Juin 2015 — Aout 2015</h4> 
-            <h5 class="place-title">Congers, NY, USA</h5>
-            <h3 class="job-title">Ingénieur Méthodes stagiaire</h3>
+            <div>
+                <h2 class="company-title">Aptar</h2>
+                <h4 class="date-title">Juin 2015 — Aout 2015</h4> 
+                <h5 class="place-title">Congers, NY, USA</h5>
+                <h3 class="job-title">Ingénieur Méthodes stagiaire</h3>
+            </div>
 
             <h2 class="project">Industrie Pharmaceutique</h2>
 
@@ -105,10 +111,12 @@
                 <li class="action">Création d’une méthode de suivi de la production assemblage</li>
             </ul>
 
-            <h2 class="company-title">Cooper Standard Automotive</h2>
-            <h4 class="date-title">Février 2012 — Mars 2012</h4> 
-            <h5 class="place-title">Częstochowa, PO</h5>
-            <h3 class="job-title">Technicien Méthodes</h3>
+            <div>
+                <h2 class="company-title">Cooper Standard Automotive</h2>
+                <h4 class="date-title">Février 2012 — Mars 2012</h4> 
+                <h5 class="place-title">Częstochowa, PO</h5>
+                <h3 class="job-title">Technicien Méthodes</h3>
+            </div>
 
             <h2 class="project">Industrie Automobile – rang 1</h2>
 
@@ -117,75 +125,93 @@
             </ul>
         </section>
 
-        <h1 class="section-title">Formation</h1>
-        <section class="IT">
-            <h2 class="section-title">IT</h2>
-            <h4 class="date-title">Juin – Septembre 2019</h4> 
-            <h5 class="place-title">Nantes, FR</h5>
-            <h3 class="job-title">Formation d’ingénieur d’études JAVA / JEE</h3>
+        <section class="formation-IT">
+            <h1 class="section-title">Formation</h1>
+            <div>
+                <h2 class="section-title">IT</h2>
+                <h4 class="date-title">Juin – Septembre 2019</h4> 
+                <h5 class="place-title">Nantes, FR</h5>
+                <h3 class="job-title">Formation d’ingénieur d’études JAVA / JEE</h3>
+            </div>
 
-            <ul class="formation-list">
-                <li class="formation">UML : Les Concepts Objets</li>
-                <li class="formation">JAVA : Les fondamentaux</li>
-                <li class="formation">Les Design Patterns</li>
-                <li class="formation">Le développement Front end :</li>
-                <ul class="formation-front-end-list">
-                    <li class="formation-front-end">HTML
-                    <li class="formation-front-end">CSS
-                    <li class="formation-front-end">Bootstrap
-                    <li class="formation-front-end">JavaScript
-                    <li class="formation-front-end">JQuery
-                    <li class="formation-front-end">Angular JS / 2+
-                    <li class="formation-front-end">Node JS
-                    <li class="formation-front-end">TypeScript</li>
-                </ul>
-                <li class="formation">Le Framework JEE et son ecosystem : </li>
-                <ul class="formation-JEE-list">
-                    <li class="formation-JEE">JSP</li>
-                    <li class="formation-JEE">SERVLETS/JSTL/EL</li>
-                    <li class="formation-JEE">JSF</li>
-                </ul>
-                <li class="formation">SPRING</li>
-                <ul class="formation-spring-list">
-                    <li class="formation-spring">Spring BATCH</li>
-                    <li class="formation-spring">Spring SECURITY</li>
-                    <li class="formation-spring">Spring MVC</li>
-                    <li class="formation-spring">Spring DATA</li>
-                </ul>
-                <li class="formation">Langage XML :</li>
-                <ul class="formation-XML-list">
-                    <li class="formation-XML">JAXB</li>
-                    <li class="formation-XML">XSD</li>
-                    <li class="formation-XML">XSL</li>
-                </ul>
-                <li class="formation">Les services et SOA :</li>
-                <ul class="formation-services-list">
-                    <li class="formation-service">JAX-RS</li>
-                    <li class="formation-service">JAX-WS</li>
-                    <li class="formation-service">Architecture REST</li>
-                </ul>
-                <li class="formation">Les ORM et SGBD</li>
-                <ul class="formation-db-list">
-                    <li class="formation-db">MySQL / Oracle</li>
-                    <li class="formation-db">PL/SQL/JPQL/HQL</li>
-                    <li class="formation-db">JPA</li>
-                    <li class="formation-db">Hibernate</li>
-                </ul>
-            </ul>
-            <ul class="formation-tool-list">
-                <li class="formation-tool"><strong>Outils : </strong>GIT, Sonar, Jenkins, Maven </li>
-                <li class="formation-tool"><strong>IDE : </strong>Eclipse, Mysql Workbench</li>
-                <li class="formation-tool"><strong>Génie Logiciel : </strong>La Qualité Logiciel</li>
-                <li class="formation-tool"><strong>Méthodes Agiles : </strong>XP, Agile (SCRUM/KANBAN), DevOps</li>
-                <li class="formation-tool"><strong>PROJET : </strong>Développement d’application Java JEE</li>
-                <li class="formation-tool"><strong>Tests : </strong>JUNIT, Selenium</li>
-            </ul>
+            <table>
+                <tr>
+                    <td>
+                        <ul>
+                            <li class="formation">UML : Les Concepts Objets</li>
+                            <li class="formation">JAVA : Les fondamentaux</li>
+                            <li class="formation">Les Design Patterns</li>
+                            <li class="formation">Le développement Front end :</li>
+                            <ul class="formation-front-end-list">
+                                <li class="formation-front-end">HTML
+                                <li class="formation-front-end">CSS
+                                <li class="formation-front-end">Bootstrap
+                                <li class="formation-front-end">JavaScript
+                                <li class="formation-front-end">JQuery
+                                <li class="formation-front-end">Angular JS / 2+
+                                <li class="formation-front-end">Node JS
+                                <li class="formation-front-end">TypeScript</li>
+                            </ul>
+                            <li class="formation">Le Framework JEE et son ecosystem : </li>
+                            <ul class="formation-JEE-list">
+                                <li class="formation-JEE">JSP</li>
+                                <li class="formation-JEE">SERVLETS/JSTL/EL</li>
+                                <li class="formation-JEE">JSF</li>
+                            </ul>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul class="formation-list">
+                            <li class="formation">SPRING</li>
+                            <ul class="formation-spring-list">
+                                <li class="formation-spring">Spring BATCH</li>
+                                <li class="formation-spring">Spring SECURITY</li>
+                                <li class="formation-spring">Spring MVC</li>
+                                <li class="formation-spring">Spring DATA</li>
+                            </ul>
+                            <li class="formation">Langage XML :</li>
+                            <ul class="formation-XML-list">
+                                <li class="formation-XML">JAXB</li>
+                                <li class="formation-XML">XSD</li>
+                                <li class="formation-XML">XSL</li>
+                            </ul>
+                            <li class="formation">Les services et SOA :</li>
+                            <ul class="formation-services-list">
+                                <li class="formation-service">JAX-RS</li>
+                                <li class="formation-service">JAX-WS</li>
+                                <li class="formation-service">Architecture REST</li>
+                            </ul>
+                            <li class="formation">Les ORM et SGBD</li>
+                            <ul class="formation-db-list">
+                                <li class="formation-db">MySQL / Oracle</li>
+                                <li class="formation-db">PL/SQL/JPQL/HQL</li>
+                                <li class="formation-db">JPA</li>
+                                <li class="formation-db">Hibernate</li>
+                            </ul>
+                        </ul>
+                    </ul>
+                    </td>
+                    <td>
+                        <ul class="formation-tool-list">
+                            <li class="formation-tool"><strong>Outils : </strong>GIT, Sonar, Jenkins, Maven </li>
+                            <li class="formation-tool"><strong>IDE : </strong>Eclipse, Mysql Workbench</li>
+                            <li class="formation-tool"><strong>Génie Logiciel : </strong>La Qualité Logiciel</li>
+                            <li class="formation-tool"><strong>Méthodes Agiles : </strong>XP, Agile (SCRUM/KANBAN), DevOps</li>
+                            <li class="formation-tool"><strong>PROJET : </strong>Développement d’application Java JEE</li>
+                            <li class="formation-tool"><strong>Tests : </strong>JUNIT, Selenium</li>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
+
         </section>
-        <section class="plastic">
+        <section class="formation-plastic">
             <h2 class="section-title">Plasturgie</h2>
-            <h4 class="date-title">2013 – 2016</h4> 
-            <h5 class="place-title">Alençon, FR</h5>
-            <h3 class="job-title">Ingénieur Polymères et composites</h3>
+            <div>
+                <h4 class="date-title">2013 – 2016</h4> 
+                <h5 class="place-title">Alençon, FR</h5>
+                <h3 class="job-title">Ingénieur Polymères et composites</h3>
+            </div>
 
             <ul class="formation-list">
                 <li class="formation">Mise en œuvre des matériaux thermoplastiques et composites</li>
@@ -194,19 +220,20 @@
                 <li class="formation">Conduite de projet & Organisation Industrielle </li>
                 <li class="formation">Lean Manufacturing</li>
             </ul>
-            
-            <h4 class="date-title">2011 – 2013</h4> 
-            <h5 class="place-title">Alençon, FR</h5>
-            <h3 class="job-title">BTS Europlastic (2ème de France)</h3>
+
+            <div>
+                <h4 class="date-title">2011 – 2013</h4> 
+                <h5 class="place-title">Alençon, FR</h5>
+                <h3 class="job-title">BTS Europlastic (2ème de France)</h3>
+            </div>
 
             <ul class="formation-list">
                 <li class="formation">Etude et application des procédés de transformations des matières plastiques</li>
                 <li class="formation">Conception de produits et d’outillages pour l’injection thermoplalstique</li>
             </ul>
         </section>
-
-        <h1 class="section-title">Divers</h1>
         <section class="miscelaenous">
+        <h1 class="section-title">Divers</h1>
             <h2 class="section-title">Langues</h2>
             <ul class="language-title-list">
                 <li class="language-title">Français</li>
--- navbar.php
+++ navbar.php
@@ -1,36 +1,33 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" media="screen" href="/assets/css/navbar.css"/>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top ">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Welcome on Etienne's Website</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/docker">Docker</a>
+<nav>
+    <p><a href="#">Welcome on Etienne's Website</a></p>
+</nav>
+<!--
+    <div>
+        <div >
+            <ul>
+                <li >
+                    <a href="/docker">Docker</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Index</a>
+                <li>
+                    <a href="/">Index</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Langue</a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li>
+                    <a href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Langue</a>
+                    <ul>
                         <li><a class="dropdown-item" href="#">Francais</a></li>
                         <li><a class="dropdown-item" href="#">English</a></li>
                     </ul>
                 </li>
-                
-                <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
+                <li>
+                    <a class="nav-link disabled" href="#" tabindex="-1">Link</a>
                 </li>
-
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <li>
+                    <a href="#" tabindex="-1">Disabled</a>
                 </li>
             </ul>
         </div>
+
     </div>
-</nav>
+-->
